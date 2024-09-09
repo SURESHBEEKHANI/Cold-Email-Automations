443dcbc13957b3833985ec54f6ca2db1c3c91f4c
Suresh Beekhani <107859372+SURESHBEEKHANI@users.noreply.github.com>
2024-09-08 22:36:45 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
 from chains import Chain
